c898c47ada09380e56c766e8212130156dd329f9
Luihxd <Luisrachm8@gmail.com>
2023-08-05 23:53:51 -0600

    componentes cabecero, egreso e ingreso funcionales y documentados

--- src/app/egreso/egreso.component.ts
+++ src/app/egreso/egreso.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { EgresoServicio } from './egreso.service';
+import { Egreso } from './egreso.model';
 
+/*app-egreso
+  Componente egreso
+  Muestra la lista de egresos, y da la opción de borrarlos
+*/
 @Component({
   selector: 'app-egreso',
   templateUrl: './egreso.component.html',
@@ -7,9 +13,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EgresoComponent implements OnInit {
 
-  constructor() { }
+  //Arreglo de ingresos.
+  egresos: Egreso[] = [];
 
+  /*Variables que vienen desde componente padre
+  app.component que inyectamos
+  gracias al "@Input()".*/
+  @Input() porcentajeTotal:number;
+  @Input() egresoTotal:number;
+
+  /*Se inyecta una dependencia del servicio de egreso para
+  inicializar el arreglo de egresos que tenemos creado en
+  en el servicio.*/
+  constructor(private egresoServicio: EgresoServicio) { }
+
+  //Inicializa el arreglo (vacío) de egresos con los
+  //egresos del arreglo del egresoServicio.
   ngOnInit(): void {
+    this.egresos = this.egresoServicio.egresos;
+  }
+
+  //Elimina del arreglo el egreso que recibe como
+  //parámetro en el html.
+  eliminarEgreso(egreso: Egreso): void{
+    this.egresoServicio.eliminarEgreso(egreso);
+  }
+
+  //Calcula el porcentaje individual de cada egreso
+  //tomando en cuenta el porcentaje total de los egresos
+  //con respecto a los ingresos.
+  calcularPorcentajeDeEgreso(egreso: Egreso): number{
+    return egreso.valor*this.porcentajeTotal/this.egresoTotal;
   }
 
 }
--- src/app/ingreso/ingreso.component.ts
+++ src/app/ingreso/ingreso.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingreso } from './ingreso.model';
+import { IngresoServicio } from './ingreso.service';
 
+/*app-ingreso
+  Componente ingreso
+  Muestra la lista de ingresos, y da la opción de borrarlos.
+*/
 @Component({
   selector: 'app-ingreso',
   templateUrl: './ingreso.component.html',
@@ -7,9 +13,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngresoComponent implements OnInit {
 
-  constructor() { }
+  //Arreglo de ingresos.
+  ingresos:Ingreso[] = [];
 
+  /*Se inyecta una dependencia del servicio de ingreso para
+  inicializar el arreglo de ingresos que tenemos creado en
+  en el servicio.*/
+  constructor(private ingresoServicio:IngresoServicio) {
+
+  }
+
+  //Inicializa el arreglo (vacío) de ingresos con los
+  //ingresos del arreglo del ingresoServicio.
   ngOnInit(): void {
+    this.ingresos = this.ingresoServicio.ingresos;
+  }
+
+  //Elimina del arreglo el ingreso que recibe como
+  //parámetro en el html.
+  eliminarIngreso(ingreso: Ingreso): void{
+    this.ingresoServicio.eliminar(ingreso);
   }
 
 }
